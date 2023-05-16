7438fa8d3bc3df05e4243ccfbe51889b683b16ac
AnasNemrawi <anasnemrawi3000@gmail.com>
2023-05-16 10:26:49 +0000

    Lab14

--- sechma.sql
+++ sechma.sql
@@ -1,7 +1,8 @@
-
-create table if not exists added_movie(
-     id serial primary key,
-     title varchar(255),
-     overview varchar(10000)
-
-)
+CREATE TABLE IF NOT EXISTS firstMOV(
+id SERIAL PRIMARY KEY,
+title VARCHAR (255),
+release_date VARCHAR (255),
+poster_path VARCHAR(255),
+overview VARCHAR(10000)
+comment VARCHAR(100000)
+);
